105e4d3c3c080d5bc81285c864099ea955501376
Not-A-Normal-Robot <76723917+Not-A-Normal-Robot@users.noreply.github.com>
2021-06-13 17:45:03 +0700

    Update Menu.cs

--- Menu.cs
+++ Menu.cs
@@ -5,36 +5,42 @@ namespace Menu
 {
     public class Main
     {
-        public static int cursorPos;
+        static int cursorPos;
+        const int maxCursorPos = 11;
+        static bool start;
         public static void Start()
         {
-            cursorPos = 0;
-            HighScores.LoadScores();
-            Controls.LoadControls();
-            Console.Write(
-                $"===================\n" +
-                $"||  RETROBLOCKS  ||\n" +
-                $"===================\n" +
-                $"{(cursorPos == 0 ? "> " : "  ")}Start\n" +
-                $"{(cursorPos == 1 ? "> " : "  ")}DAS: {Controls.das}\n" +
-                $"{(cursorPos == 2 ? "> " : "  ")}ARR: {Controls.arr}\n" +
-                $"{(cursorPos == 3 ? "> " : "  ")}Soft Drop Mode: {(Controls.useSonicDrop ? "instant" : "normal")}\n" +
-                $"Controls: \n"+
-                $"{(cursorPos == 4 ? "> " : "  ")}Left: {Controls.left}\n" +
-                $"{(cursorPos == 5 ? "> " : "  ")}Left: {Controls.right}\n" +
-                $"{(cursorPos == 6 ? "> " : "  ")}Left: {Controls.hardDrop}\n" +
-                $"{(cursorPos == 7 ? "> " : "  ")}Left: {Controls.softDrop}\n" +
-                $"{(cursorPos == 8 ? "> " : "  ")}Left: {Controls.rotCw}\n" +
-                $"{(cursorPos == 9 ? "> " : "  ")}Left: {Controls.rotCcw}\n" +
-                $"{(cursorPos == 10? "> " : "  ")}Left: {Controls.rot180}\n" +
-                $"{(cursorPos == 11? "> " : "  ")}Left: {Controls.hold}\n" +
-                $"Scores:\n" +
-                $"{HighScores.Scores[0]}\n" +
-                $"{HighScores.Scores[1]}\n" +
-                $"{HighScores.Scores[2]}\n" +
-                $"{HighScores.Scores[3]}\n" +
-                $"{HighScores.Scores[4]}\n"
-                );
+            while (!start)
+            {
+                cursorPos = 0;
+                HighScores.LoadScores();
+                Controls.LoadControls();
+                Console.Write(
+                    $"===================\n" +
+                    $"||  RETROBLOCKS  ||\n" +
+                    $"===================\n" +
+                    $"{(cursorPos == 0 ? "> " : "  ")}Start\n" +
+                    $"Handling: \n"+
+                    $"{(cursorPos == 1 ? "> " : "  ")}DAS: {Controls.das}\n" +
+                    $"{(cursorPos == 2 ? "> " : "  ")}ARR: {Controls.arr}\n" +
+                    $"{(cursorPos == 3 ? "> " : "  ")}Soft Drop Mode: {(Controls.useSonicDrop ? "instant" : "normal")}\n" +
+                    $"Controls: \n" +
+                    $"{(cursorPos == 4 ? "> " : "  ")}Left: {Controls.left}\n" +
+                    $"{(cursorPos == 5 ? "> " : "  ")}Left: {Controls.right}\n" +
+                    $"{(cursorPos == 6 ? "> " : "  ")}Left: {Controls.hardDrop}\n" +
+                    $"{(cursorPos == 7 ? "> " : "  ")}Left: {Controls.softDrop}\n" +
+                    $"{(cursorPos == 8 ? "> " : "  ")}Left: {Controls.rotCw}\n" +
+                    $"{(cursorPos == 9 ? "> " : "  ")}Left: {Controls.rotCcw}\n" +
+                    $"{(cursorPos == 10 ? "> " : "  ")}Left: {Controls.rot180}\n" +
+                    $"{(cursorPos == 11 ? "> " : "  ")}Left: {Controls.hold}\n" +
+                    $"Scores:\n" +
+                    $"{HighScores.Scores[0]}\n" +
+                    $"{HighScores.Scores[1]}\n" +
+                    $"{HighScores.Scores[2]}\n" +
+                    $"{HighScores.Scores[3]}\n" +
+                    $"{HighScores.Scores[4]}\n"
+                    );
+            }
         }
     }
 }
